730932b648b2fb8697a06818808814d940579239
Rickson <ricksonrocha0@gmail.com>
2021-11-12 23:04:38 -0300

    Criando projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import ApiService from './ApiService/service'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  useEffect(()=>{
+    const load = async () => {
+      try {
+        const response = await ApiService.getFilmes()
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    load();
+  })
+  return(
+    <div>Qualquer coisa</div>
+  )
 }
 
-export default App;
+export default App
