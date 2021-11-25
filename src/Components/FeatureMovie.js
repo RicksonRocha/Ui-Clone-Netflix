d5de6cfa3427fdc9f81ae82f9ab238572095487b
Rickson <ricksonrocha0@gmail.com>
2021-11-25 17:54:06 -0300

    Alteracoes

--- src/Components/FeatureMovie.js
+++ src/Components/FeatureMovie.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-import { Featured, ImageFillHorizontal, ImageFillVertical, Name, Info, Points, Year, Seasons, Description, Genres } from './FeatureMovie.styles'
+import { Featured, ImageFillHorizontal, ImageFillVertical, Name, Info, Points, Year, Seasons, Description, BtnArea, Genres } from './FeatureMovie.styles'
 
 export default ({ item }) => {
+
+    let firstDate = new Date(item.first_air_date)
+    let genres = []
+    for (let i in item.genres) {
+        genres.push(item.genres[i].name)
+    }
 
     return (
         <Featured style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})` }}>
@@ -10,12 +16,16 @@ export default ({ item }) => {
                 <ImageFillHorizontal>
                     <Name>{item.original_name}</Name>
                     <Info>
-                        <Points>{item.vote_average}</Points>
-                        <Year></Year>
-                        <Seasons>{item.number_of_seasons} temporada{item.number_of_seasons != 1 ? 's' : ''}</Seasons>
+                        <Points>{item.vote_average} pontos</Points>
+                        <Year>{firstDate.getFullYear()}</Year>
+                        <Seasons>{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</Seasons>
                     </Info>
                     <Description>{item.overview}</Description>
-                    <Genres><strong>Gênero:</strong></Genres>
+                    <BtnArea>
+                        <a href={`/watch/${item.id}`} theme="light">&#x27A4; Assistir</a>
+                        <a href={`/list/add/${item.id}`} theme={"dark"}>+ Minha Lista</a>
+                    </BtnArea>
+                    <Genres><strong>Gênero{genres.length > 1 && 's'}:</strong> {genres.join(', ')}</Genres>
                 </ImageFillHorizontal>
             </ImageFillVertical>
         </Featured>
--- src/Components/FeatureMovie.styles.js
+++ src/Components/FeatureMovie.styles.js
@@ -18,10 +18,45 @@ export const ImageFillHorizontal = styled.div`
     background: linear-gradient(to right, #111 20%, transparent 60%);
 `
 
-export const Name = styled.div``
-export const Info = styled.div``
-export const Points = styled.div``
-export const Year = styled.div``
+export const Name = styled.div`
+    font-size: 60px;
+    font-weight: bold;
+`
+export const Info = styled.div`
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 15px;
+
+    > * {
+        margin-right: 15px;
+        display: inline-block; //lado a lado, com propriedade width
+    }
+`
+export const Points = styled.div`
+    color: #46d369;
+`
+export const Year = styled.div`
+    
+`
 export const Seasons = styled.div``
-export const Description = styled.div``
+export const Description = styled.div`
+    margin-top: 15px;
+    font-size: 20px;
+    color: #999;
+    max-width: 40%;
+`
+export const BtnArea = styled.div`
+    margin-top: 15px;
+
+    a {
+        display: inline-block;
+        font-size: 20px;
+        font-weight: bold;
+        padding: 15px;
+        border-radius: 5px;
+        text-decoration: none;
+        margin-right: 15px;
+        background-color: ${(props) => props.theme === 'light' ? '#fff' : '#000'};
+    }
+`
 export const Genres = styled.div``
